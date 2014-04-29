b57601c024255f71d7c322b77524ed2db3c2fa6e
Nick Desaulniers <ndesaulniers@mozilla.com>
2014-04-29 16:18:49 -0700

    pull fetch into its own module

--- server.js
+++ server.js
@@ -2,11 +2,8 @@
 
 var connect = require('connect');
 var express = require('express');
-var glob = require('glob');
 var lodash = require('lodash');
 var stylus = require('stylus');
-var path = require('path');
-var fs = require('fs');
 var _ = require('lodash');
 var json2csv = require('json2csv');
 
@@ -17,62 +14,7 @@ app.set('view engine', 'jade');
 app.use(connect.compress());
 app.use(express.static(__dirname));
 
-function normalizeSlug(slug) {
-	return slug.replace(/_\d+$/g, '');
-}
-
-var ignoreReasons = /MULTIPLE_APPS_PER_ORIGIN_FORBIDDEN|REINSTALL_FORBIDDEN/;
-
-function fetch() {
-	var results = {};
-	var i = 0;
-	var all = JSON.parse(fs.readFileSync('reports/manifest.json'));
-	_.forEach(all, function(app) {
-		var slug = normalizeSlug(app.app_name);
-		results[slug] = {
-			idx: i++,
-			name: app.app_name,
-			slug: slug,
-			runs: [],
-			passed: 0,
-			failed: 0,
-			screenshots: [],
-			logs: []
-		};
-	});
-
-	var files = glob.sync('reports/*/*.json');
-	files.forEach(function(file) {
-		var data = fs.readFileSync(file);
-		var partial = JSON.parse(data);
-		for (var name in partial) {
-			var slug = normalizeSlug(name);
-			var app = results[slug];
-			if (!app) {
-				console.warn('Skipped %s from %s', slug, file);
-				continue;
-			}
-			var entry = partial[name];
-			var run = {};
-			run.status = !(entry.status && entry.status.indexOf('FAILED') == 0);
-			run.from = file;
-			run.logcat = path.dirname(file) + '/' + entry.logcat;
-			if (!run.status) {
-				if (ignoreReasons.test(entry.status)) {
-					continue;
-				}
-				run.reason = entry.status.substr(8);
-				app.failed++;
-			} else {
-				app.passed++;
-				app.screenshots.push('/' + path.dirname(file) + '/' + entry.screenshot);
-			}
-			app.status = app.passed && app.passed >= app.failed;
-			app.runs.push(run);
-		}
-	});
-	return results;
-}
+var fetch = require('./fetch.js');
 
 app.get('/', function(req, res) {
 	// res.setHeader('content-type', 'text/plain');
