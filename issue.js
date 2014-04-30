4546053fe48c8615616ec64599cb1ffaf65d76dd
Nick Desaulniers <ndesaulniers@mozilla.com>
2014-04-30 13:08:07 -0700

    add count

--- issue.js
+++ issue.js
@@ -1,7 +1,8 @@
 var results = require('./fetch')();
 //console.log(JSON.stringify(results, null, 2));
 
-// { errorName: ['appNames']}
+var timeoutRE = /TimeoutException/;
+// { errorName: { apps: ['appNames'], count: 0 } }
 var errors = {};
 Object.keys(results).forEach(function (appName) {
   var app = results[appName];
@@ -9,14 +10,20 @@ Object.keys(results).forEach(function (appName) {
   if (app.failed > 0) {
     //console.log(app.name + " had an error: " + JSON.stringify(app.runs, null, 2));
     app.runs.forEach(function (run) {
-      if (!errors[run.reason]) {
-        errors[run.reason] = [];
+      if (!run.reason) return;
+      if (timeoutRE.test(run.reason)) {
+        run.reason = "failed to install: TimeoutException: Timed out after 120.X seconds\n";
       }
-      if (errors[run.reason].indexOf(app.name) === -1) {
-        errors[run.reason].push(app.name);
+      if (!errors[run.reason]) {
+        errors[run.reason] = { apps: [], count: 0 };
+      }
+      if (errors[run.reason].apps.indexOf(app.name) === -1) {
+        errors[run.reason].apps.push(app.name);
+        ++errors[run.reason].count;
       }
     });
   }
 });
 
 console.log(JSON.stringify(errors, null, 2));
+
